0392567022c9bc292704ac1bbdfbf1b0eec85a0b
ajewe <ajewe@utexas.edu>
2020-04-23 21:11:23 -0500

    appbar

---
+++ src/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <AppBar position="relative" style={{ background: '#3CB371' }}>
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: "1" }}>Austin Small Business</Typography>
+        <Typography>
+          <ul className="nav-list">
+            <li className="nav-list-item">
+              {/* <Link to="/">Listings</Link> */}
+              Listings
+            </li>
+            <li className="nav-list-item">
+              {/* make the below conditionally rendered  */}
+              {/* <Link to="/login">Login</Link> */}
+              Login
+            </li>
+          </ul>
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navigation
