cb1c9129c93595341e01ae3c84d38692a5f3f8da
ajewe <ajewe@utexas.edu>
2020-05-05 20:50:11 -0500

    Updates

---
+++ precache-manifest.b1a59dda4bdf11303b183a751f469bbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea87eaa596843e3eaec33106fa46d691",
+    "url": "/ATX-small-biz-app/index.html"
+  },
+  {
+    "revision": "c83067ab11ebce2d0196",
+    "url": "/ATX-small-biz-app/static/css/main.b1eb0e3a.chunk.css"
+  },
+  {
+    "revision": "aeff65aa943296e3fee7",
+    "url": "/ATX-small-biz-app/static/js/2.2b8f7a76.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/ATX-small-biz-app/static/js/2.2b8f7a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83067ab11ebce2d0196",
+    "url": "/ATX-small-biz-app/static/js/main.1136dbe7.chunk.js"
+  },
+  {
+    "revision": "0ff21784b9796ebc61e0",
+    "url": "/ATX-small-biz-app/static/js/runtime-main.5e3ed2b1.js"
+  }
+]);
